43f5ef7b79647f11a3094972517050616ef4e2bc
Saeed Desouky <saeeddesouky71@gmail.com>
2022-06-03 19:17:53 +0200

    fix examples

--- public/boards/rapbit32/blocks/generators_ir.js
+++
@@ -1,6 +0,0 @@
-Blockly.Python['ir_read'] = function(block) {
-  Blockly.Python.definitions_['import_IR'] = 'import ir';
-
-  var code = `ir.read()`;
-  return [code, Blockly.Python.ORDER_NONE];
-};
